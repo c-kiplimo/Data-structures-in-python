19f0a7694e2b76dde4c9141aaefab6263e561cca
c-kiplimo <kiplimocollins855@gmail.com>
2024-02-10 21:49:53 +0200

    implement reverse numbers

--- Recursion/Numbers__reversing_a_number.py
+++ Recursion/Numbers__reversing_a_number.py
@@ -1,16 +1,16 @@
-def digits(num):
-    d=1
-    while(num//10!=0):
-        d+=1
-        num=num//10
-    return d
+def digits(num):#function to find the number of digits in a number
+    d=1#initializing the number of digits to 1
+    while(num//10!=0):#loop to find the number of digits
+        d+=1#incrementing the number of digits
+        num=num//10#reducing the number
+    return d#returning the number of digits
  
-def rev_number(num):
-    if(num//10==0):
-        return num
+def rev_number(num):#function to reverse a number
+    if(num//10==0):#base case
+        return num#returning the number if it has only one digit
     else:
-        d=digits(rev_number(num//10))
-        num1=(num%10)*(10**d)+rev_number(num//10)
+        d=digits(rev_number(num//10))#finding the number of digits in the number
+        num1=(num%10)*(10**d)+rev_number(num//10)#reversing the number
         return num1
 
 print(rev_number(12345))
